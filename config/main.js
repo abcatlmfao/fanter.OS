dd2baad3fedadf78f9a9de08eb368de82db6089e
ChipikiPal800 <161179292+ChipikiPal800@users.noreply.github.com>
2026-04-13 21:22:00 -0500

    site name change

--- config/main.js
+++ config/main.js
@@ -1,6 +1,6 @@
 // This changes the title of your site
 
-var sitename = "gaym."; // Change this to change the name of your website.
+var sitename = "fanter."; // Change this to change the name of your website.
 var subtext = "v1.989898"; // set the subtext
 
 // more settings in main.css
